a99ea96822223cc70b0b11c47869ba1ea3f112da
Gyuyoung Lee <ifos2u@naver.com>
2021-03-08 16:57:00 +0900

    Update [06]_2.py

--- Solutions/[06]_2.py
+++ Solutions/[06]_2.py
@@ -1,28 +1,21 @@
+# 시간제한이 추가되서 유효한 코드로 변경이 필요합니다
 def solve(n, x, y):
     global result
-    if n == 2:
+    if n == 1:
         if x == X and y == Y:
             print(result)
-            return
-        result += 1
-        if x == X and y + 1 == Y:
-            print(result)
-            return
-        result += 1
-        if x + 1 == X and y == Y:
-            print(result)
-            return
-        result += 1
-        if x + 1 == X and y + 1 == Y:
-            print(result)
-            return
         result += 1
         return
-    solve(n / 2, x, y)
-    solve(n / 2, x, y + n / 2)
-    solve(n / 2, x + n / 2, y)
-    solve(n / 2, x + n / 2, y + n / 2)
+
+    n //= 2
+    for i, j in [(x, y), (x, y+n), (x+n, y), (x+n, y+n)]:
+        # 가지치기 추가: 4분면 중 1곳만 탐색한다
+        if (i <= X < i+n) and (j <= Y < j+n):
+            solve(n, i, j)
+        else:
+            result += n * n
+
 
 result = 0
-N, X, Y = map(int, input().split(' '))
+N, X, Y = map(int, input().split())
 solve(2 ** N, 0, 0)
